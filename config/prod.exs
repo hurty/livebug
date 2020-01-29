b2ef4c11302b2d0824a8dac099493749be1856ca
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2020-01-29 17:45:02 +0100

    Add check origin

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :livebug, LivebugWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "livebug.herokuapp.com", port: 443],
+  check_origin: ["https://livebug.herokuapp.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
